f022d4ec12871e230a1710c39a25ef05633f5ac7
Jahnavi <janvi.sankari@gmail.com>
2026-08-03 21:37:24 +0530

    ✅ PYTH09 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/PYTH09/solution.py
+++ codechef/medium/PYTH09/solution.py
@@ -1,4 +1,3 @@
-# Update the '_' below to solve the problem
-
-print(11*13)          # Area of the rectangle
-print(2 * (11+13))    # Perimeter of the rectangle
+# Update the '_' in the code below to provide the following output
+# 7 3
+print(3 + 4, 2 + 1)
