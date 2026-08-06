3b8a927442389791ab2f50ccf93bd2c626d02a73
Jahnavi <janvi.sankari@gmail.com>
2026-08-06 18:41:41 +0530

    ✅ LPYASM25 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-06

--- codechef/medium/LPYASM25/solution.py
+++ codechef/medium/LPYASM25/solution.py
@@ -1,4 +1,7 @@
 # cook your dish here
-m=5
-n=6
-print(m**n)
+time1=1430
+time2=1615
+minutes1=(time1//100)*60+(time1%100)
+minutes2=(time2//100)*60+(time2%100)
+difference=minutes2-minutes1
+print(difference)
