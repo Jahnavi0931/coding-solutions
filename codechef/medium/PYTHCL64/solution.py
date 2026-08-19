a38a894c1f3077c4fb347e798250609ae83c251a
Jahnavi <janvi.sankari@gmail.com>
2026-08-19 21:04:29 +0530

    ✅ PYTHCL64 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/PYTHCL64/solution.py
+++ codechef/medium/PYTHCL64/solution.py
@@ -1,7 +1,9 @@
-# Update your code below this line
+r = 1000
+w = 3222
+if r > w:
+#This code will not run due to improper indentation
+    print("White balls are out of stock")  
+else:
+#Fix the error by putting a space before both print
+    print("Your order is Confirmed")
 
-a=int(input())
-b=int(input())
-if a>b:
-    print("Coding is Fun")
-    
