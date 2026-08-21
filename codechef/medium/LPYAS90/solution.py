bd9cee9ee743c2a2fc37b1ce3720fa6e30a1dd52
Jahnavi <janvi.sankari@gmail.com>
2026-08-21 20:32:22 +0530

    ✅ LPYAS90 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/LPYAS90/solution.py
+++ codechef/medium/LPYAS90/solution.py
@@ -1,4 +1,11 @@
-# Debug and fix the code
-str = input()
-n = len(str)
-print(str[-1]) 
+# Update the code below to debug the problem
+choice = int(input())
+
+if choice == 1:
+    print("Option 1 selected")
+elif choice == 2:
+    print("Option 2 selected")
+elif choice == 3:
+    print("Option 3 selected")
+else:
+    print("invalid choice")
