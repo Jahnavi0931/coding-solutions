a36d5d9c0c8ee1c3bdca2d435f694e665dc242f5
Jahnavi <janvi.sankari@gmail.com>
2026-08-25 22:53:45 +0530

    ✅ PYTH101 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/medium/PYTH101/solution.py
+++ codechef/medium/PYTH101/solution.py
@@ -1,6 +1,12 @@
-# Solution as follows
+# Debug the code below to output the factiorial of a user defined input n
 
 n = int(input())
 
-for i in range(1, 11):
-    print(n, "x", i, "=", n*i)
+i = 1
+factorial = 1
+
+while i <= n:
+    factorial = i* factorial
+    i = i + 1
+    
+print("The factorial of the given number is:", factorial)
