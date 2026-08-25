b3608428443fd70dad88d8be45332cc77b7b8877
Jahnavi <janvi.sankari@gmail.com>
2026-08-25 22:49:46 +0530

    ✅ PYTH99A | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/medium/PYTH99A/solution.py
+++ codechef/medium/PYTH99A/solution.py
@@ -1,6 +1,12 @@
-# Solution as follows
+# Count how many 'o's are present in the string using a 'for' loop and 'if' condition
 
-numbers = [1, 6, 4, 3, 2, 5]
+string = 'bolloon'
 
-for i in numbers:
-    print(i)
+# use this variable to count occurrences of o
+count_o = 0      
+for i in string:
+    if i == 'o':
+        # iterate through the string, everytime you find an 'o', increase count_o by 1
+        count_o = count_o + 1
+
+print(count_o)
