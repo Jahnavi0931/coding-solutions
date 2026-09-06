fea727bf856b2e1898a1583b3aeda0140c734728
Jahnavi <janvi.sankari@gmail.com>
2026-09-06 20:20:24 +0530

    ✅ GSQ04 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/GSQ04/solution.sql
@@ -0,0 +1,9 @@
+/* Write a query to create a table 'employee', with columns employee_id, employee_Name and Department. 
+Update the blanks below to solve this problem */
+
+CREATE TABLE employee
+(
+    Employee_id int, /* Integer Data type assigned to the variable */
+    Employee_Name   char(20), /* TEXT Data type assigned to the variable */
+    Department  char(20) /* TEXT Data type assigned to the variable */
+);
