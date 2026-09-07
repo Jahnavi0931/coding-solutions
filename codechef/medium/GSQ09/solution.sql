bd5406d2b424e1f52970f33c78a01a699e1bcc0e
Jahnavi <janvi.sankari@gmail.com>
2026-09-07 20:15:56 +0530

    ✅ GSQ09 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/GSQ09/solution.sql
@@ -0,0 +1,7 @@
+/*Write a query to set the Department as 'HR', for the employee with employee_id 2 to the existing table employee. */
+update employee
+set department="hr"
+where employee_id=2;
+select*from employee;
+
+
